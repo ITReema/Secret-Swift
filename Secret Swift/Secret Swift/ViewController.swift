83974b819a973b1284f6f382a0b14fc297310f0b
Reema <it.reema7@gmail.com>
2019-06-20 23:58:54 +0300

    Add files via upload

---
+++ Secret Swift/Secret Swift/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Secret Swift
+//
+//  Created by mac_os on 17/10/1440 AH.
+//  Copyright © 1440 mac_os. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var secret: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func authenticateTapped(_ sender: Any) {
+    }
+    
+}
+
